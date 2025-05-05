1e9ef4fd6eb3e4a6f55574b149b1a1ec308c20c5
M-KE24 <mikeangelosumido1993@gmail.com>
2025-05-05 20:13:32 +0800

    Update scriptnba.js

--- scriptnba.js
+++ scriptnba.js
@@ -1,35 +1,3 @@
-function sendMessage() {
-    const userInput = document.getElementById('user-input');
-    const messageText = userInput.value.trim();
-
-    if (messageText === '') {
-        return;
-    }
-
-    const chatBox = document.getElementById('chat-box');
-    const userMessage = document.createElement('div');
-    userMessage.textContent = messageText;
-    userMessage.classList.add('chat-message', 'sent');
-
-    chatBox.appendChild(userMessage);
-
-    // Simulate response from chatbot after a delay
-    setTimeout(() => {
-        const chatbotResponse = document.createElement('div');
-        chatbotResponse.textContent = '';
-        chatbotResponse.classList.add('chat-message', 'received');
-
-        chatBox.appendChild(chatbotResponse);
-
-        // Scroll to bottom of chat box
-        chatBox.scrollTop = chatBox.scrollHeight;
-    }, 1000);
-
-    userInput.value = ''; // Clear user input after sending message
-}
-
-
-
 document.addEventListener("DOMContentLoaded", async () => {
             const video = document.getElementById('player');
             const youtubeEmbed = document.getElementById('youtube-player');
